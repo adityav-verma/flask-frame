5ea8780dc986e6b7d03ce41c2cb2743b32cb15fb
coderadi <meetadi3@gmail.com>
2018-01-16 01:36:56 +0530

    Add configure_blueprints to register various blueprints with an app

--- app/app.py
+++ app/app.py
@@ -3,6 +3,17 @@ from flask import Flask
 # For import *
 __all__ = ['create_app']
 
+
 def create_app():
+    """Create an app instance based on the passed params"""
     app = Flask(__name__)
+    configure_blueprints(app)
     return app
+
+
+def configure_blueprints(app):
+    """Register all blueprints with the app"""
+    # TODO: Figure out why . is needed
+    from .api import api
+    for bp in [api]:
+        app.register_blueprint(bp)
