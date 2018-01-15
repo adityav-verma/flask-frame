0f23ff442fb642993155e130844049b202f11211
coderadi <meetadi3@gmail.com>
2018-01-16 00:39:53 +0530

    Move setup related configs out of app
    - requirements is moved to root
    - uwsgi.ini is moved to root

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.6
 
-COPY ./app /app
+COPY . /www/app
+
+WORKDIR /www/app
+
+ENV UWSGI_INI /www/app/uwsgi.ini
 
 # Install dependencies
 RUN pip install -r requirements.txt
--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('settings')
-
 
 @app.route('/')
 def index():
     return 'Hello World'
-
 
 if __name__ == "__main__":
     # Only for debugging while developing
