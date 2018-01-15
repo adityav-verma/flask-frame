2d92cdcaa58767df849b2cbcf196fe7a952740bf
coderadi <meetadi3@gmail.com>
2018-01-16 00:30:43 +0530

    Add up things to a working state

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM tiangolo/uwsgi-nginx-flask:python3.6
 
 COPY ./app /app
 
+# Install dependencies
 RUN pip install -r requirements.txt
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('settings')
+
 
 @app.route('/')
 def index():
     return 'Hello World'
 
+
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80, threaded=True)
+    app.run(host='0.0.0.0', debug=True, port=8002, threaded=True)
