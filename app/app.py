196c97be5e50b820bb00a4e3d89da8509507845c
coderadi <meetadi3@gmail.com>
2018-01-16 02:00:20 +0530

    Load configs on app creation

--- app/app.py
+++ app/app.py
@@ -1,19 +1,37 @@
 from flask import Flask
+from .configs import DefaultConfig
 
 # For import *
 __all__ = ['create_app']
 
 
-def create_app():
+def create_app(config=None, app_name=None):
     """Create an app instance based on the passed params"""
-    app = Flask(__name__)
+    if not app_name:
+        app_name = DefaultConfig.APP_NAME
+
+    app = Flask(app_name)
+
+    configure_app(app, config)
     configure_blueprints(app)
+
     return app
+
+
+def configure_app(app, config=None):
+    """
+    Load config to the app and add additional configurations if needed
+    http://flask.pocoo.org/docs/0.12/api/#configuration
+    """
+    # Laod the default config
+    app.config.from_object(DefaultConfig)
+
+    if config:
+        app.config.from_object(config)
 
 
 def configure_blueprints(app):
     """Register all blueprints with the app"""
-    # TODO: Figure out why . is needed
     from .api import api
     for bp in [api]:
         app.register_blueprint(bp)
