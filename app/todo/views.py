b436423dfbb8b63dc114add175644de8badf7fd8
coderadi <meetadi3@gmail.com>
2018-04-13 23:56:47 +0530

    Add a sample POST endpoint

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, jsonify
+from app.todo.models.todo import Todo
 
 todo = Blueprint('todo', __name__, url_prefix='/todo')
 
 @todo.route('/', methods=['GET'])
 def index():
     return jsonify({'message': 'Hello World'})
+
+@todo.route('/', methods=['POST'])
+def new():
+    todo = Todo.add('Yo!', 'Jessi Pinkman')
+    return jsonify({'Title': todo.title})
