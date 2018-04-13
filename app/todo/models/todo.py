e31d767c395117e93d14709cea088087f631b600
coderadi <meetadi3@gmail.com>
2018-04-13 23:56:04 +0530

    Add helper functions to Todo Model
    - add (classmethod) to create a new object
    - _commit(instance method) to commit an instance change to DB

--- app/todo/models/todo.py
+++ app/todo/models/todo.py
@@ -7,3 +7,16 @@ class Todo(db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     title = db.Column(db.String(500), nullable=False)
     content = db.Column(db.Text())
+
+    def __str__(self):
+        return ('Id: {}, Title: {}'.format(self.id, self.title))
+
+    def _commit(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @classmethod
+    def add(cls, title, content):
+        todo = cls(title=title, content=content)
+        return todo._commit()
