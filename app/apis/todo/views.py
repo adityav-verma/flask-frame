60874498972edbd69e7c4d6bf15feb5a62031480
coderadi <meetadi3@gmail.com>
2018-05-08 23:19:22 +0530

    Allow Todo addition only for a logged in user.

--- app/apis/todo/views.py
+++ app/apis/todo/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request
 
 from app.utilities import ApiResult
+from app.extensions import oauth
 from app.utilities.schema_validators import validate_request_schema
+
 from .schemas import NewTodoSchema
 from .models.todo import Todo
 
@@ -10,18 +12,21 @@ todo = Blueprint('todo', __name__, url_prefix='/api/todo')
 
 
 @todo.route('/', methods=['GET'])
+@oauth.require_oauth('email')
 def index():
+    user = request.oauth.user
     return ApiResult(
-        payload={},
-        message='Hello from Python.. hzzzzz..'
+        payload={[todo.to_dict() for todo in user.todos]},
+        message='Listing all todos'
     )
 
 
 @todo.route('/', methods=['POST'])
+@oauth.require_oauth('email')
 @validate_request_schema(schema=NewTodoSchema)
 def new():
     data = request.json
-    todo = Todo.add(data['title'], data['content'])
+    todo = Todo.add(data['title'], data['content'], request.oauth.user)
     return ApiResult(
         payload=todo.to_dict(),
         message='Todo created!',
